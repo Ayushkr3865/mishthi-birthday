00c6f04faa2bf0f420f8ec0e961703fbaa6a32d4
Ayush kumar ray <ak05078189@gmail.com>
2025-06-23 18:55:31 +0530

    added

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -157,7 +157,7 @@ export default function MainContent() {
             <div className="bg-white rounded-xl p-6 shadow-md overflow-y-auto flex-1 custom-scrollbar">
                 <div className="relative z-10">
                     <div className="text-gray-700 text-lg leading-relaxed mb-4">
-                        <p>From the very first moment our paths crossed, life has felt more beautiful, more exciting, and more meaningful. Every smile, every conversation, and every memory we've shared has built a story so precious that no words can truly capture it. I feel incredibly grateful to have someone like you by my side, someone who understands me without even saying a word, someone who brings light even on the darkest days. Through ups and downs, laughter and tears, you've been a constant source of strength and happiness. We continue to write our journey together, I want you to always remember that you are deeply cherished, endlessly appreciated, and truly loved. Here's to the countless memories we've yet to create and the beautiful future that awaits us. You are, and always will be, my favorite part of every day. Or haa aap bolte hue achi lagti ho bolti raha karo. Dusro ko buri lagti hongi aapki baatein but I can be your good listner even in your hard time.💖</p>
+                        <p>From the very first moment our paths crossed, life has felt more beautiful, more exciting, and more meaningful. Every smile, every conversation, and every memory we've shared has built a story so precious that no words can truly capture it. I feel incredibly grateful to have someone like you by my side, someone who understands me without even saying a word, someone who brings light even on the darkest days. Through ups and downs, laughter and tears, you've been a constant source of strength and happiness. We continue to write our journey together, I want you to always remember that you are deeply cherished, endlessly appreciated, and truly loved. Here's to the countless memories we've yet to create and the beautiful future that awaits us. You are, and always will be, my favorite part of every day. Or haa aap bolte hue achi lagti ho bolti raha karo. Dusro ko buri lagti hongi aapki baatein but I can be your good listner even in your hard time.</p>
                     </div>
                 
                 </div>
@@ -177,7 +177,7 @@ export default function MainContent() {
                     transition={{ repeat: Infinity, duration: 2 }}
                     className="text-6xl mb-8"
                 >
-                    ❤️
+                    
                 </motion.div>
                 <motion.button
                     whileHover={{ scale: 1.05 }}
